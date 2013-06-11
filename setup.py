b60debf8e8e911ace472346c69de40fbc9d307b1
Nejc Zupan <nejc.zupan@gmail.com>
2013-06-11 18:18:49 +0200

    Preparing release 1.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = \
 
 setup(
     name='niteoweb.ipn.jvzoo',
-    version='1.2',
+    version='1.3',
     description="JVZoo IPN support in Plone.",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
